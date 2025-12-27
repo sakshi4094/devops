8852bba36228dd4d0da0d47ad98705e0e359d4eb
Sakshi Dongare <sakshi4094@users.noreply.github.com>
2025-12-27 23:55:51 +0530

    instace is intial level

--- sample1.tf
+++ sample1.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 resource "aws_instance" "myinstance" {
     ami = "ami-0ecb62995f68bb549"
-    instance_type = "t2.micro"
+    instance_type = "t3.micro"
     vpc_security_group_ids = ["sg-0cf1dd7789ca04235"]
     key_name = "task-key"
     availability_zone = "us-east-1a"
