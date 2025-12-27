1ab0dce0692c07fdc1bdb02018eccab121182d3d
Sakshi Dongare <sakshi4094@users.noreply.github.com>
2025-12-27 23:30:37 +0530

    intial instace creation

--- sample1.tf
+++ sample1.tf
@@ -2,10 +2,11 @@ provider "aws" {
     region = "us-east-1" 
 }
 resource "aws_instance" "myinstance" {
-    ami = "ami-0c398cb65a93047f2"
+    ami = "ami-0ecb62995f68bb549"
     instance_type = "t2.micro"
     vpc_security_group_ids = ["sg-0cf1dd7789ca04235"]
     key_name = "task-key"
+    availability_zone = "us-east-1a"
     tags = {
       name ="myinstance"
     }
